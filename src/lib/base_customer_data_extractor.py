df6ab4f1326d2605a011f4eb2b238de5e25575e6
Manveer <msaulak@gmail.com>
2021-01-16 17:50:49 +1100

    cleaning logic completed

--- src/lib/base_customer_data_extractor.py
+++ src/lib/base_customer_data_extractor.py
@@ -3,12 +3,14 @@ import os
 
 import pandas as pd
 from typing import Optional
+
 logger = logging.getLogger('pi.exchange.emailsender')
+
 
 class BaseCustomerDataExtractor:
     def __init__(self, data_file_path: str, errors_file_path: str):
-        self.data_file_path:str = data_file_path
-        self.errors_file_path:str = errors_file_path
+        self.data_file_path: str = data_file_path
+        self.errors_file_path: str = errors_file_path
 
         self.customer_data: Optional[pd.DataFrame] = None
         self.erroneous_data: Optional[pd.DataFrame] = None
@@ -18,15 +20,21 @@ class BaseCustomerDataExtractor:
 
     def _clean_dataset(self):
         self.erroneous_data = self.customer_data[self.customer_data.isnull().any(axis=1)]
-
+        logger.info(f'Erroneous records\n{self.erroneous_data}')
         # Write to file
         error_file_directory = os.path.dirname(self.errors_file_path)
-        print(error_file_directory)
+
+        logger.info(f'Creating directories upto {error_file_directory}')
         os.makedirs(error_file_directory, exist_ok=True)
 
-        self.erroneous_data.c
+        logger.info(f'Exporting erroneus records to {self.errors_file_path}')
+        self.erroneous_data.to_csv(self.errors_file_path, index=False)
+
+        # Overwrite customer data without the erroneous records
+        self.customer_data.dropna(inplace=True)
 
     def load_dataset(self):
         self.customer_data = self._load_customer_data()
-        logger.info(f'Customer data\n{self.customer_data}')
+        logger.info(f'Customer dataframe\n{self.customer_data}')
         self._clean_dataset()
+        logger.info(f'Cleaned customer data\n{self.customer_data}')
